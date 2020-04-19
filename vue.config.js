b5ad65600251c637be4c550fcd7104aa84df6f5d
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-19 20:42:34 +0200

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const ExtractTextPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  devServer: {
+    port: '8081',
+  },
+  chainWebpack: (config) => {
+    config.optimization.delete('splitChunks')
+
+    config.output
+    .filename('[name].js')
+
+    config.plugin('extract-css')
+    .use(ExtractTextPlugin, [{
+      filename: '[name].css',
+      allChunks: true,
+    }])
+  },
+  configureWebpack: { // double ?
+    output: {
+      filename: '[name].js',
+    },
+  },
+  lintOnSave: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/scss/style.scss";
+        `,
+      },
+    },
+  },
+}
